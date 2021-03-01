1bc1eb39778a165fdd8e89e4f78df19ccc3b4f39
aguirguis <arsany.guirguis@epfl.ch>
2021-03-01 19:20:21 +0100

    marking working version with ResNet50

--- inference_playground.py
+++ inference_playground.py
@@ -3,21 +3,22 @@ from swiftclient.service import SwiftService, SwiftPostObject, SwiftError
 container = 'cifar10'
 objects=['cifar-10-batches-py/test_batch'] #'cifar-10-batches-py/data_batch_1']
 swift = SwiftService()
-step = 1000					#current limits: 9K with Cifarnet, 850 with ResNet50
-for start in range(0,1000,step):
+step = 10000					#current limits: 9K with Cifarnet, 850 with ResNet50
+for start in range(0,10000,step):
   end = start+step
   end = 10000 if end >= 10000 else end
   #ask to do inference for images [strat:end] from the test batch
   print("Inference for data [{}:{}]".format(start,end))
   opts = {"meta": {"Ml-task:inference",
-	"dataset:cifar10","model:cifarnet",
+	"dataset:cifar10","model:resnet50",
 	"start:{}".format(start),"end:{}".format(end)}}
   post_objects=[SwiftPostObject(o,opts) for o in objects]
   for post_res in swift.post(
       container=container,
       objects=post_objects):
     if post_res['success']:
-      inf_res = str(post_res['result'])[1:-1].split()
+#      print("length of raw inference result: ", len(post_res['result']))
+      inf_res = str(post_res['result'])[1:-1].split(",")
       print("Inference result length: ", len(inf_res))
       print("Object '%s' POST success" % post_res['object'])
     else:
--- swift_rebuild.sh
+++ swift_rebuild.sh
@@ -2,5 +2,6 @@
 swift-init all stop		#stop swift
 cd ~/swift; sudo python3 setup.py develop		#rebuild swift
 cd ~/swift; sudo chown -R ${USER}:${USER} swift.egg-info
-startmain			#start swift
+swift-init main start
+#startmain			#start swift
 echo "" > ../swift_personal_log.txt	#clear logs
