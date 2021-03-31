f5a1849e4710078809326f432418467ddf0de32a
aguirguis <arsany.guirguis@epfl.ch>
2021-04-01 01:41:09 +0200

    add backward step wasted time to plotting the models

--- mymodels_playground/plot_models_info.py
+++ mymodels_playground/plot_models_info.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from myresnet import build_my_resnet
 from myvgg import build_my_vgg
 from mydensenet import build_my_densenet
@@ -6,6 +7,7 @@ from myalexnet import build_my_alexnet
 from myinception import build_my_inception
 from mymnasnet import build_my_mnasnet
 from mymobilenetv2 import build_my_mobilenetv2
+import time
 ###For plotting
 import numpy as np
 import matplotlib
@@ -21,8 +23,9 @@ all_models = ["alexnet", 'densenet121','densenet161','densenet169','densenet201'
 		"resnet18",'resnet34','resnet50','resnet101','resnet152','resnext50_32x4d',
 	        'resnext101_32x8d','wide_resnet50_2','wide_resnet101_2', "inception"]
 
-model_to_idx = {"alexnet": [0], "densenet":np.arange(1,5), "mobilenet":[5], 
-		"mnasnet":np.arange(6,10), "vgg":np.arange(10,14), "resnet1":np.arange(14,19), "resnet2":np.arange(19,23), "inception":[23]}
+model_to_idx = {"alexnet": [0], "densenet":np.arange(1,5), "mobilenet":[5],
+	"vgg":np.arange(10,14), "resnet1":np.arange(14,19), "resnet2":np.arange(19,23),
+	"mnasnet":np.arange(6,10), "inception":[23]}
 def build_model(model_str, num_classes):
   if model_str.startswith('alex'):
     model = build_my_alexnet(num_classes)
@@ -41,6 +44,7 @@ def build_model(model_str, num_classes):
   return model
 
 width=0.4
+criterion = nn.CrossEntropyLoss()
 for mod_class, idxs in model_to_idx.items():
   a = torch.rand((1,3,224,224))
   for ii,idx in enumerate(idxs):
@@ -51,8 +55,17 @@ for mod_class, idxs in model_to_idx.items():
     print("{}{}{}".format("="*20,model_str,"="*20))
     if model_str == "inception":
       a = torch.rand((2,3,299,299))
-    net = build_model(model_str, 1000)
-    _, sizes, times = net(a, 0,150)		#This will print some stuff
+    num_classes=1000
+    net = build_model(model_str, num_classes)
+    res, sizes, times = net(a, 0,150)		#This will print some stuff
+    target = torch.tensor((10,))
+    back_time = time.time()
+    loss = criterion(res, target)
+    loss.backward()
+    back_time = time.time()-back_time
+    print("Forward iteration takes: ", np.sum(times))
+    print("Backward iteration takes: ", back_time)
+    times.append(back_time)
     sizes[0] = 132			#correct the input size (before rescaling)...we know that onr ImageNet image is 132 KB on average
     times.insert(0,0)			#basically the input layer takes no time
     times = np.array(times)*1000
@@ -60,6 +73,7 @@ for mod_class, idxs in model_to_idx.items():
     ax2.set_yticks(np.arange(0,np.max(times),step=np.max(times)/10))
     fig = ax1.bar(ind-0.5*width, sizes, width, linewidth=1, label=model_str+"-data-size",hatch="/",edgecolor='black',)
     figs.append(fig)
+    ind = np.append(ind, len(ind))		#to match the backward time
     fig3 = ax2.bar(ind+0.5*width, times, width, linewidth=1, color='orange', label=model_str+"-latency",hatch="\\",edgecolor='black',)
     figs.append(fig3)
     fig2 = ax1.axhline(132, color='r', linestyle="--", label='input size')
